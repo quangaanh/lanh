05c1f1d8bcf6749fcaece8039ea59a4b86a8a90f
quangaanh <nhockten0121@gmail.com>
2024-09-08 05:16:15 +0900

    first commit

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Babe à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'Dạ....',
+    title: 'Tớ xin lỗi vì đã làm phiền cậu nhưng mà nay tớ có chuyện muốn nói với cậu !!',
+    desc: 'Tớ thực sự thích cậu 1 cẩm giác rất lạ đến cả tớ còn không thể giải thích được !!',
+    btnYes: 'Tớ hiểu r',
+    btnNo: '!!',
+    question:'Tớ muốn nói chuyện với cậu nếu cậu không phiền nhé <3',
+    btnReply: 'Gửi',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'CẢM ƠN BBI tớ sẽ trả lời cậu ngay.',
+    messDesc: 'Xn lỗi bbi nếu như tớ trả lời muộn nhé',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/nguyen.quang.0804' 
+}
